81f05dc2fdce95a7338b5b0ce92cc4c89d3aad5d
Della Hall <della.hall@raccoon.pizza>
2020-08-07 21:36:55 -0600

    adding JSON support

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -1,25 +1,63 @@
+#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
-#include <SFML/System.hpp>
+
+#include "nlohmann/json.hpp"
+
+using json = nlohmann::json;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
+  json j2 = {
+    {"pi", 3.141},
+    {"happy", true},
+    {"name", "Niels"},
+    {"nothing", nullptr},
+    {"answer", {
+        {"everything", 42}
+    }},
+    {"list", {1, 0, 2}},
+    {"object", {
+        {"currency", "USD"},
+        {"value", 42.99}
+    }}
+  };
+  // Create the main window
+  sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+  // Load a sprite to display
+  sf::Texture texture;
+  if (!texture.loadFromFile("graphics/stfu.png"))
+      return EXIT_FAILURE;
+  sf::Sprite sprite(texture);
+  // Create a graphical text to display
+  sf::Font font;
+  if (!font.loadFromFile("fonts/Hack-Bold.ttf"))
+      return EXIT_FAILURE;
+  sf::Text text("Hello SFML", font, 50);
+  // Load a music to play
+  sf::Music music;
+  if (!music.openFromFile("sound/File_Mozart_-_Coronation_Mass_(Markevitch)_-_01._Kyrie.ogg"))
+      return EXIT_FAILURE;
+  // Play the music
+  //music.play();
+  // Start the game loop
+  while (window.isOpen())
+  {
+    // Process events
+    sf::Event event;
+    while (window.pollEvent(event))
     {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
+        // Close window: exit
+        if (event.type == sf::Event::Closed)
+            window.close();
     }
-
-    return 0;
+    // Clear screen
+    window.clear();
+    // Draw the sprite
+    window.draw(sprite);
+    // Draw the string
+    window.draw(text);
+    // Update the window
+    window.display();
+  }
+  return EXIT_SUCCESS;
 }
